9e6b32e672e884193766634fa94a014e25f83c4b
Joshua Hebert <jhebertcocktails@gmail.com>
2023-09-14 20:42:33 -0400

    Changed file structure and added assets folder with images needed for the portfolio.

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,16 +1,30 @@
-import PortfolioContainer from "./components/PortfolioContainer";
-import Footer from "./components/Footer";
-import './App.css';
+// Import React components
+import React from 'react';
 
-
-
+// Import main components
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import NavBar from './components/NavBar/NavBar';
+// Import Views components
+import About from './components/About/About';
+import Contact from './components/Contact/Contact'
+import Home from '../src/components/Home';
+import Portfolio from './components/Portfolio/Portfolio';
+import Resume from './components/Resume/Resume';
+// Create portfolioContainer component
 function App() {
   return (
     <div className="App">
-      <PortfolioContainer />
+      <Header />
+      <NavBar />
+      <Home />
+      <About />
+      <Portfolio />
+      <Resume />
+      <Contact />
       <Footer />
     </div>
   );
-}
-
+};
+// Export portfolioContainer component
 export default App;
