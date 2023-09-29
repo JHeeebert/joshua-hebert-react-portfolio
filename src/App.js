a7e7dfa7cad66454d6f4b52caf51dca70da7e7c4
Joshua Hebert <jhebertcocktails@gmail.com>
2023-09-29 11:11:28 -0400

    added about.js logic to produce about me page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Header from "../src/components/Header/Header.js";
-// import About from "../src/components/About/About.js";
+import About from "../src/components/About/About.js";
 // import Portfolio from "../src/components/Portfolio/Portfolio.js";
 // import Contact from "../src/components/Contact/Contact.js";
 // import Resume from "../src/components/Resume/Resume.js";
@@ -19,10 +19,11 @@ const App = () => {
         selectedSection={selectedSection}
         handleSectionChange={handleSectionChange}
       />
-      {/* {selectedSection === "About Me" && <About />}
-      {selectedSection === "Portfolio" && <Portfolio />}
+      {selectedSection === "About Me" && <About />}
+
+      {/* {selectedSection === "Portfolio" && <Portfolio />}
       {selectedSection === "Contact" && <Contact />}
-      {selectedSection === "Resume" && <Resume />} */}
+      {selectedSection === "Resume" && <Resume />} */} 
        <Footer />
 
     </div>
---
+++ src/components/About/About.js
@@ -0,0 +1,38 @@
+import React from "react";
+import profileImage from "../../assets/profile.jpg";
+
+const AboutMe = () => {
+  return (
+    <section className="about-me">
+      <img className="profile-image" src={profileImage} alt="Profile" />
+      <div className="about-me-content">
+            <h2>About Me</h2>
+            <p>
+                Hey there! I'm Joshua Hebert, a dedicated Full Stack Web Developer
+                hailing from the vibrant east coast of the United States. My tech
+                toolkit includes React.js, Express.js, Node.js, and MongoDB, allowing me
+                to craft exceptional digital experiences.
+            </p>
+            <p>
+                From a young age, I've been fascinated by the art of coding and its
+                transformative potential. This curiosity led me to dive into the world
+                of Full Stack Web Development, where I discovered a passion for creating
+                dynamic, user-centric applications. I thrive on challenges and am
+                continually evolving to stay at the forefront of this ever-evolving tech
+                landscape.
+            </p>
+            <div className="interests">
+                <h3>Interests</h3>
+                <ul>
+                    <li>🏀 Avid sports enthusiast</li>
+                    <li>🎮 Gaming aficionado</li>
+                    <li>🍴 Exploring new culinary experiences</li>
+                    <li>📸 Capturing moments through photography</li>
+                </ul>
+            </div>
+            </div>
+        </section>
+    );
+};
+
+export default AboutMe;
