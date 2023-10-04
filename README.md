d83de6b2a73d47200b83f63b3523c2bbb201f83d
Joshua Hebert <jhebertcocktails@gmail.com>
2023-10-04 10:17:48 -0400

    updated aboutme and readme

--- README.md
+++ README.md
@@ -1,18 +1,29 @@
 # Joshua Hebert React Portfolio
+
 ## Description
-As an employer looking for candidates with experience building single-page applications, I want to view a potential employee's deployed React portfolio of work samples so that I can assess whether they're a good candidate for an open position.
-## Acceptance Criteria
-- **Given** a single-page application portfolio for a web developer:
-  - **When** I load the portfolio:
-    - **Then** I am presented with a page containing a header, a section for content, and a footer.
-  - **When** I view the header:
-    - **Then** I am presented with the developer's name and navigation with titles corresponding to different sections of the portfolio.
-  - ... (follows the acceptance criteria)
-# Table of Contents
+
+As an experienced web developer passionate about creating engaging and seamless user experiences, my React portfolio showcases my dedication to crafting exceptional single-page applications. Through this portfolio, I invite you to explore a curated selection of my work, gaining insight into my skills, creativity, and attention to detail.
+
+From the moment you land on the main page, you'll be greeted with a user-friendly interface that reflects my commitment to intuitive design. The navigation seamlessly guides you through different sections, each offering a glimpse into various aspects of my professional journey.
+
+## What My Portfolio Represents
+
+- **Technical Proficiency**: The projects in my portfolio demonstrate my proficiency in React, showcasing how I utilize this powerful library to build interactive and dynamic web applications.
+
+- **Problem-Solving Abilities**: In every project, I tackle unique challenges, employing innovative solutions that speak to my problem-solving capabilities.
+
+- **Versatility and Adaptability**: My diverse range of projects highlights my ability to adapt to different technologies and project requirements, showcasing versatility in my skill set.
+
+- **Passion for Learning and Growth**: Through my portfolio, you'll see not just completed projects but a continuous journey of learning and improvement. I'm dedicated to staying up-to-date with the latest technologies and frameworks.
+
+- **User-Centric Focus**: I prioritize the end-user experience in all my projects. Intuitive design, seamless navigation, and engaging interfaces are at the core of my development approach.
+
+## Table of Contents
+
 - [Joshua Hebert React Portfolio](#joshua-hebert-react-portfolio)
   - [Description](#description)
-  - [Acceptance Criteria](#acceptance-criteria)
-- [Table of Contents](#table-of-contents)
+  - [What My Portfolio Represents](#what-my-portfolio-represents)
+  - [Table of Contents](#table-of-contents)
   - [Installation](#installation)
   - [Usage](#usage)
   - [Screenshots](#screenshots)
@@ -21,23 +32,39 @@ As an employer looking for candidates with experience building single-page appli
   - [Questions](#questions)
 
 ## Installation
+
 - Clone the repository: `git clone https://github.com/jheeebert/joshua-hebert-react-portfolio.git`
 - Navigate to the project directory: `cd joshua-hebert-react-portfolio`
 - Install dependencies: `npm install`
+
 ## Usage
+
 - Run the app: `npm start`
 - Open your browser and visit the [Joshua Hebert React Portfolio](https://jheeebert.github.io/joshua-hebert-react-portfolio/)
+
 ## Screenshots
+
 - **Main Page** <br>
   ![Main Page](./screenshots/mainPage.png)
 - **Portfolio Image** <br>
   ![Portfolio](./screenshots/portfolioImage.png)
-- ... (add more screenshots and descriptions)
+  - **Portfolio Image2** <br>
+  ![Portfolio2](./screenshots/portfolioImage2.png)
+    - **Contact** <br>
+  ![Contact](./screenshots/contactImage.png)
+      - **Resume** <br>
+  ![Resume](./screenshots/resumeImage.png)
+
 ## License
+
 [![GitHub license](https://badgen.net/github/license/Naereen/Strapdown.js)](https://github.com/Naereen/StrapDown.js/blob/master/LICENSE)
+
 ## Contributing
+
 We welcome contributions! Please follow the steps outlined in the [Contributors Covenant](https://www.contributor-covenant.org/).
+
 ## Questions
+
 - Ask anything: [Ask Me Anything](https://GitHub.com/jheeebert/joshua-hebert-react-portfolio)
 - Contact: Email me at jheeebertwd@gmail.com
 - More of our work: [GitHub Profile](https://github.com/jheeebert/)
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,34 +2,60 @@ import React from "react";
 import profileImage from "../../assets/profile.jpg";
 
 const AboutMe = () => {
-  return (
-    <section className="about-me">
-      <img className="profile-image" src={profileImage} alt="Profile" />
-      <div className="about-me-content">
-            <h2>About Me</h2>
-            <p>
-                Hey there! I'm Joshua Hebert, a dedicated Full Stack Web Developer
-                hailing from the Queen city, Charlotte North Carolina. My tech
-                toolkit includes React.js, Express.js, Node.js, MySQL, GraphQL and MongoDB, allowing me
-                to craft seamless digital experiences.
-            </p>
-            <p>
-                From a young age, I've been fascinated by the art of coding and its
-                transformative potential. This curiosity led me to dive into the world
-                of Full Stack Web Development, where I discovered a passion for creating
-                dynamic, user-centric applications. I thrive on challenges and am
-                continually evolving to stay at the forefront of this ever-evolving tech
-                landscape.
-            </p>
-            <div className="interests">
-                <h3>Interests</h3>
-                <ul>
-                    <li>🏀 Avid sports enthusiast</li>
-                    <li>🎮 Gaming aficionado</li>
-                    <li>🍴 Exploring new culinary experiences</li>
-                    <li>📸 Capturing moments through photography</li>
-                </ul>
-            </div>
+    return (
+        <section className="about-me">
+            <img className="profile-image" src={profileImage} alt="Profile" />
+            <div className="about-me-content">
+                <h2>About Me</h2>
+                <p>
+                    Greetings! I’m Joshua Hebert, a passionate Full Stack Web Developer
+                    based in the vibrant city of Charlotte, North Carolina. My journey
+                    into the world of web development was sparked by an early fascination
+                    with technology, which soon blossomed into a dedicated pursuit. I’ve
+                    had the privilege of acquiring a diverse set of skills, including
+                    proficiency in React.js, Express.js, Node.js, MySQL, GraphQL, and
+                    MongoDB. With these tools in my arsenal, I craft seamless digital
+                    experiences that seamlessly marry form and function.
+                </p>
+                <p>
+                    What truly ignites my enthusiasm for this field is the power it holds
+                    to drive meaningful change. From the elegant architecture of a
+                    well-designed website to the intricacies of back-end functionality,
+                    every facet of development represents an opportunity to innovate and
+                    create impactful solutions. In my constant quest for growth, I am not
+                    just a developer; I am an innovator, a problem solver, and a lifelong
+                    learner. The dynamism of the tech industry fuels my drive to
+                    continually evolve and adapt, ensuring that I stay ahead of the curve.
+                </p>
+                <div className="interests">
+                    <h3>Interests</h3>
+                    <ul>
+                        <li>
+                            <h4>🏀 Avid Sports Enthusiast:</h4> Beyond the screen, I’m an avid sports
+                            enthusiast, both as a player and a fervent spectator. Sports teach
+                            us about teamwork, strategy, and determination—values that
+                            resonate in the tech world as well.
+                        </li>
+                        <li>
+                        <h4>🎮 Gaming Aficionado:</h4> In the realm of gaming, I find not only
+                            entertainment but also a fascinating intersection of technology
+                            and storytelling. Exploring virtual worlds and tackling in-game
+                            challenges nourishes my creative and strategic thinking.
+                        </li>
+                        <li>
+                        <h4>🍴 Culinary Explorer:</h4> The world of flavors beckons, and I am
+                            always eager to explore it. Whether it’s trying new recipes or
+                            discovering unique eateries, the culinary realm is an adventure in
+                            itself.
+                        </li>
+                        <li>
+                        <h4> 📸 Photography Enthusiast:</h4> Capturing moments through the lens is a
+                            passion that allows me to freeze memories in time. Each photograph
+                            tells a story, and this art form enables me to express my
+                            creativity and view the world from a different perspective.
+                        </li>
+                    </ul>
+                </div>
             </div>
         </section>
     );
